a12f3e29bf4ab46ce0fcc9d75078eab2c5861053
cd155 <cd155@163.com>
2023-06-26 17:43:24 -0400

    add set matrix zeros

--- main/neetcode75.py
+++ main/neetcode75.py
@@ -1431,3 +1431,39 @@ def reorderList(head):
     first, second = temp1, temp2
   
   printNode(head)
+
+'''
+45. Set Matrix Zeroes
+
+neetcode75.setZeroes([[1,1,1],[1,0,1],[1,1,1]]) -> 
+  [[1,0,1],[0,0,0],[1,0,1]]
+'''
+def setZeroes(matrix):
+  rowZero = 1
+
+  for i in range(len(matrix)):
+    for j in range(len(matrix[i])):
+      if matrix[i][j] == 0:
+        matrix[0][j] = 0
+        if i == 0:
+          rowZero = 0
+        else:
+          matrix[i][0] = 0
+  
+  for i in range(1, len(matrix)):
+    if matrix[i][0] == 0:
+      matrix[i] = [0]*len(matrix[i])
+
+  for j in range(1, len(matrix[0])):
+    if matrix[0][j] == 0:
+      for i in range(len(matrix)):
+        matrix[i][j] = 0
+
+  if matrix[0][0] == 0:
+    for i in range(len(matrix)):
+        matrix[i][0] = 0
+
+  if rowZero == 0:
+    matrix[0] = [0]*len(matrix[i])
+    
+  return matrix
