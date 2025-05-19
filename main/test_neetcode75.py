810259eee93552cf5c190710dd8a99eedc8e847f
cd155 <cd155@163.com>
2025-05-18 21:42:47 -0400

    add two sum test cases

--- main/test_neetcode75.py
+++ main/test_neetcode75.py
@@ -1,10 +1,20 @@
 import unittest
-from neetcode75 import twoSumNeetcode, twoSum
+from neetcode75 import two_sum_return_values, two_sum_return_indexes, two_sum_return_all_index_pairs
 
 class TestTwoSum(unittest.TestCase):
 
-    def test_twoSumNeetcode_basic(self):
-        self.assertEqual(twoSumNeetcode([2,7,11,15], 9) , (2, 7))
+    def test_two_sum_return_indexes(self):
+        self.assertEqual(two_sum_return_indexes([2,7,11,15], 9) , (0, 1))
+        self.assertEqual(two_sum_return_indexes([2,7,11,15,1,1,3], 2) , (4, 5))
+        self.assertEqual(two_sum_return_indexes([], 9) , None)
+        self.assertEqual(two_sum_return_indexes([2], 9) , None)
+        self.assertEqual(two_sum_return_indexes([2,7,11,15], 10) , None)
+
+    def test_two_sum_exclusive_pairs(self):
+        self.assertEqual(two_sum_return_all_index_pairs([4,4,8,10,0,8,4], 8), [[0,1], [2,4], [4,5],[0,6]])
+
+    def test_two_sum_values(self):
+        self.assertEqual(two_sum_return_values([2,7,11,15], 9) , (2, 7))
 
 if __name__ == '__main__':
     unittest.main()
